15c6d00dd9ce6a572312d4c518882a0f9cfc4d5c
fasquelito <alexis.fsq@gmail.com>
2013-03-13 17:15:55 +0100

    ajout question 8

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
-//#include "lire.c"
+#include "lire.c"
+
+#define MAX_FACTORS 64
 
 //---------------------------------------------------------------
 //					Fonctions utiles
@@ -28,12 +30,57 @@ int is_prime( uint64_t p )
 				return 0;
 			}
 		}
-
 		return 1;
 	}
 }
 
+//Question 8
+int get_prime_factors ( uint64_t n, uint64_t *factors_tab )
+{
+	int index_factors = 0;
+	int i = 2;
+	int reste = n;
+	
+	//On parcours tous les nombres jusqu'à ce que le reste soit premier.
+	while ( ! is_prime(reste) )
+	{
+		//Si c'est un nombre premier
+		if ( is_prime( i ) )
+		{
+			//Tant qu'il est divisible par le premier
+			while ( reste % i == 0 )
+			{
+				//On stocke le resultat de la division dans reste
+				reste = reste/i;
+
+				//On sauvegarde le nombre
+				factors_tab[index_factors++] =  reste;
+			}
+		}
+
+		i++;
+	}
+	factors_tab[index_factors++] = reste;
+	
+	return index_factors;
+}
+
 //Affiche la factoriasation en nombre premier
+void print_prime_factor_bis ( uint64_t n )
+{
+	uint64_t factors[MAX_FACTORS];
+	int j, k;
+	
+	k = get_prime_factors ( n, factors );
+	
+	printf ( "%llu : ", n );
+	for ( j = 0 ; j < k ; j ++ )
+	{
+		printf ( "%llu ", factors[j] );	
+	}
+	printf ( "\n" );
+}
+
 void print_prime_factors( uint64_t n )
 {
 	int i = 2;
